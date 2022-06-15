244bcd868e3852ac5143a3daa49a9a113bffb4b1
mzhans <98946477+mzhans@users.noreply.github.com>
2022-06-15 14:06:59 +0700

    memasukkan file tugas-1

---
+++ tugas-1.js
@@ -0,0 +1,28 @@
+//Array Nama Lengkap
+const myName = ["Aren", "Kurnia"];
+
+//Map()
+const newName = myName.map(function (myName){
+    return "1" + myName;
+});
+console.log(newName);
+
+//Filter()
+const filter = newName.filter(name => name.includes("i"));
+console.log(filter);
+
+//Find()
+const myNickname = newName.find(name => name = "e");
+console.log(myNickname);
+
+//Function welcome() dan parameter callback
+function welcome(callback){
+    console.log("Selamat Datang ");
+    callback();
+};
+
+//Panggil function welcome() dengan argumen berupa anonymous function 
+//yang mencetak nilai dari variabel myNickname
+welcome(function(){
+    console.log(myNickname);
+});
