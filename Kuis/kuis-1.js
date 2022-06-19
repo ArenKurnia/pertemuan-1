db0e3372879aa2245199e3452e687671bb677fd0
mzhans <98946477+mzhans@users.noreply.github.com>
2022-06-19 10:09:10 +0700

    commit dulu

---
+++ Kuis/kuis-1.js
@@ -0,0 +1,22 @@
+const namaSaya = ["Aren", "Kurnia", "Cantik"];
+const map = namaSaya.map(function (namaSaya){
+    return namaSaya + "a";
+});
+console.log(map);
+
+// [namaDepan, namaTengah, namaBelakang] = namaSaya;
+// console.log(namaDepan, namaTengah, namaBelakang);
+
+// const mahasiswa = {
+//     namaLengkap : "Aren Kurniaa Cantika",
+//     umur        : 21
+// }
+// console.log(mahasiswa);
+
+// const{namaLengkap, umur} = mahasiswa;
+// console.log(namaLengkap);
+// console.log(umur);
+
+
+
+
